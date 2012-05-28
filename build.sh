2c1bd9816f42cb3c3d7f57a6636db363ce29e229
Jonathan Clark <jon.h.clark@gmail.com>
2012-05-28 01:29:06 -0400

    fsc has a nasty bug in which the build directory is cached incorrectly

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ scriptDir=$(readlink -f $(dirname $0))
 
 mkdir -p bin
 echo >&2 "Compiling..."
-fsc -deprecation -d bin sametime.scala
+scalac -deprecation -d bin sametime.scala
 
 echo >&2 "Building JAR $scriptDir/sametime.jar"
 (cd $scriptDir/bin; zip -qr $scriptDir/sametime.jar *)
